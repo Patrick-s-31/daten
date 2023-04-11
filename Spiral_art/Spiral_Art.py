afcf1140ebad9613bb3d451ad661c702f6f4926a
Patrick-s-31 <patrick.schittenhelm31@gmail.com>
2023-04-11 21:23:54 +0200

    project finished

--- Spiral_art/Spiral_Art.py
+++ Spiral_art/Spiral_Art.py
@@ -1,13 +1,12 @@
+import os
 import numpy as np
 from numpy import asarray
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 from PIL import Image
 import surf2stl
-#import PyMesh
-#from pymesh import stl
-import igl
 import pyvista as pv
+import bpy
 
 
 
@@ -30,14 +29,15 @@ angle = np.zeros((resolution));
 amplitude = np.zeros((resolution));
 
 #import image
-image = Image.open('test_2.jpg')
+image_name = 'test.jpg'
+image = Image.open(image_name)
 A = asarray(image)
 A = A[:,:,0]
 
 
 a = 5/(2*np.pi)
 theta = spiral_size/(2*a) # total theta at end of spiral
-x_scale = A.shape[0]/spiral_size * 1.25
+x_scale = A.shape[1]/spiral_size * 1.25
 y_scale = A.shape[1]/spiral_size * 1.25
 angle[0] = 0
 amplitude[0] = 3
@@ -75,35 +75,17 @@ Y_in = amplitude_in*np.sin(angle)
 
 Z_bottom = np.zeros((resolution))
 Z_top = np.repeat(height,resolution)
+Z_top.shape
+Z_bottom.shape
 
-#print(Z_top.shape)
-
-
+print('Creating spiral facets')
 #Generate surfaces and write to .stl files
 
 #Top surface
-X_body = np.append([X_in.T] , [X_out.T],axis=0)
-Y_body = np.append([Y_in.T], [Y_out.T],axis=0)
-Z_body = np.append([Z_top.T] , [Z_top.T],axis=0)
-surf2stl.write('Top.stl', X_body, Y_body, Z_body)
-
-#Inside surface
-X_body = np.append([X_in.T] , [X_in.T],axis=0)
-Y_body = np.append([Y_in.T], [Y_in.T],axis=0)
-Z_body = np.append([Z_top.T] , [Z_bottom.T],axis=0)
-surf2stl.write('Inside.stl', X_body, Y_body, Z_body)
-
-#Bottom surface
-X_body = np.append([X_in.T] , [X_out.T],axis=0)
-Y_body = np.append([Y_in.T], [Y_out.T],axis=0)
-Z_body = np.append([Z_bottom.T] , [Z_bottom.T],axis=0)
-surf2stl.write('Bottom.stl', X_body, Y_body, Z_body)
-
-#Outside surface
-X_body = np.append([X_out.T] , [X_out.T],axis=0)
-Y_body = np.append([Y_out.T], [Y_out.T],axis=0)
-Z_body = np.append([Z_top.T] , [Z_bottom.T],axis=0)
-surf2stl.write('Outside.stl', X_body, Y_body, Z_body)
+X_body = np.concatenate(([X_in.T], [X_out.T], [X_out.T], [X_in.T], [X_in.T]),axis=0)
+Y_body = np.concatenate(([Y_in.T], [Y_out.T], [Y_out.T], [Y_in.T], [Y_in.T]),axis=0)
+Z_body = np.concatenate(([Z_top.T], [Z_top.T],  [Z_bottom.T],  [Z_bottom.T],  [Z_top.T]),axis=0)
+surf2stl.write('Shell.stl', X_body, Y_body, Z_body)
 
 #Front surface
 X_body = np.array([[X_in[0], X_out[0]],[X_in[0], X_out[0]]])
@@ -120,47 +102,130 @@ surf2stl.write('Back.stl', X_body, Y_body, Z_body)
 
 #import surfaces, stitch surfaces and correct normals
 
-Top = pv.PolyData("Top.stl")
-Inside = pv.PolyData("Inside.stl")
-Bottom = pv.PolyData("Bottom.stl")
-Outside = pv.PolyData("Outside.stl")
+Shell = pv.PolyData("Shell.stl")
 Front = pv.PolyData("Front.stl")
 Back = pv.PolyData("Back.stl")
 
 
-merged_surface = Top + Inside + Bottom + Outside + Front + Back
+merged_surface = Shell + Front + Back
 
 merged_surface.flip_normals()
 #merged_surface.clean()
-#merged_surface.plot_normals(faces=False)
-
-plot = pv.Plotter()
-#plot.add_mesh(merged_surface, color='gray',)
-plot.set_background(color=[0.9, 0.9, 0.9])
-
-
+#merged_surface.plot_normals(faces=True)
 merged_surface.save("Full_Body.stl")
-#plot.add_mesh(merged_surface ,color='gray' ,show_edges=True)
 
-#Create Hexagons and resulting parts
 
-hex = pv.Cylinder(center=(0.0, 0.0, height/2), direction=(0.0, 0.0, 1.0), radius=(75)/np.cos(30*np.pi/180), height=height+1, resolution=6, capping=True).triangulate().subdivide(4,"linear")
+# Import the STL file
+bpy.ops.import_mesh.stl(filepath="Full_Body.stl")
 
-temp = merged_surface.boolean_intersection(hex).fill_holes(5).clean()
-#plot.add_mesh(temp ,color='gray' ,show_edges=True)
-#plot.show()
+stl_obj = bpy.context.selected_objects[0]
 
-hex1 = pv.Cylinder(center=(0.0, 0.0, height/2), direction=(0.0, 0.0, 1.0), radius=75/np.cos(30*np.pi/180), height=2.2, resolution=6, capping=True).triangulate().subdivide(4,"linear",)
-hex2 = pv.Cylinder(center=(0.0, 0.0, height/2), direction=(0.0, 0.0, 1.0), radius=(75-0.8)/np.cos(30*np.pi/180), height=3.0, resolution=6, capping=True).triangulate().subdivide(4,"linear")
-hex_boundary = hex1.boolean_difference(hex2).clean().fill_holes(30)
-hex_boundary.save("boundary.stl")
 
-temp.boolean_union(hex_boundary)#.clean().fill_holes(3)
-plot.add_mesh(temp ,color='gray' ,show_edges=True)
-#plot.add_mesh(hex_boundary ,color='gray' ,show_edges=True)
-#plot.add_mesh(hex)
-plot.show()
 
-temp.save("part1.stl")
+# Set the dimensions of the hexagonal cylinder
+height_hex = height+2
+width_hex = 150 #distance between two parallel sides
+Diameter_hex = width_hex/(np.cos(30*np.pi/180)) #Diameter of Circle outside Hex
+
+#create grid of points, where hexagons will be
+num_points = round(spiral_size / width_hex)+2 #in x and y respectively
+
+hex_grid_x = np.zeros((num_points,num_points))
+hex_grid_y = np.zeros((num_points,num_points))
+
+for i in range(num_points):
+    for j in range(num_points):
+
+        if i%2 == 0:
+            hex_grid_x[i,j] = j*width_hex
+        else:
+            hex_grid_x[i,j] = j*width_hex + width_hex/2
+
+        hex_grid_y[i,j] = -i*width_hex*np.cos(30*np.pi/180)
+
+
+#shift grid to be centered
+shift_x = num_points*width_hex/2
+shift_y = num_points*(width_hex*np.cos(30*np.pi/180))/2 + (width_hex*np.cos(30*np.pi/180)/2-Diameter_hex/4)
+hex_grid_x = hex_grid_x - shift_x
+hex_grid_y = hex_grid_y + shift_y
+
+#create parts folder, delete contents if it already exists
+if not os.path.exists('parts'):
+    os.mkdir('parts')     
+else:
+    for f in os.listdir('parts'):
+        os.remove(os.path.join('parts', f))
+
+k = 0
+#loop through all hex positions
+for i in range(hex_grid_x.shape[0]):
+        
+    for j in range(hex_grid_x.shape[1]):
+
+        if np.sqrt(hex_grid_x[i,j]**2 + hex_grid_y[i,j]**2) > 1.1*spiral_size/2: #no hexagons outside of spiral
+            pass
+        else:
+            k+=1
+            # Create the hexagonal cylinder
+            bpy.ops.mesh.primitive_cylinder_add(vertices=6, radius=Diameter_hex/2, depth=height_hex)
+
+            # Move the hexagonal cylinder to the desired position
+            bpy.ops.transform.translate(value=(hex_grid_x[i,j], hex_grid_y[i,j], height/2))
+
+            # Rotate the hexagonal cylinder
+            #bpy.ops.transform.rotate(value=0.523599, orient_axis='Z')
+
+            # Get the hex object
+            hex_obj = bpy.context.selected_objects[0]
+            #hex_obj.select_set(False)
+
+            # Add a Boolean modifier to the imported STL object
+            bool_mod1 = hex_obj.modifiers.new(name='bool1', type='BOOLEAN')
+            bool_mod1.operation = 'INTERSECT'
+            bool_mod1.object = stl_obj
+
+            # Apply the Boolean modifier
+            bpy.ops.object.modifier_apply(modifier='bool1')
+
+
+            # Select the intersected mesh
+            #intersected_mesh = stl_obj.data
+            #bpy.context.view_layer.objects.active = intersected_mesh
+            #stl_obj.select_set(True)
+
+
+            # Create the hexagonal boundary
+            outside_diameter = Diameter_hex
+            inside_diameter = Diameter_hex-1.6/np.cos(30*np.pi/180)
+
+            bpy.ops.mesh.primitive_cylinder_add(vertices=6, radius=inside_diameter/2, depth=height)
+            inside_hex = bpy.context.selected_objects[0]
+            bpy.ops.mesh.primitive_cylinder_add(vertices=6, radius=outside_diameter/2, depth=height)
+            outside_hex = bpy.context.selected_objects[0]
+
+            #Difference between inside and outside hex
+            bool_mod2 = outside_hex.modifiers.new(name='bool2', type='BOOLEAN')
+            bool_mod2.operation = 'DIFFERENCE'
+            bool_mod2.object = inside_hex
+
+            bpy.ops.object.modifier_apply(modifier='bool2')
+
+            bpy.ops.transform.translate(value=(hex_grid_x[i,j], hex_grid_y[i,j], height/2))
+
+            #Union of boundary and spiral part
+            bool_mod2 = outside_hex.modifiers.new(name='bool3', type='BOOLEAN')
+            bool_mod2.operation = 'UNION'
+            bool_mod2.object = hex_obj
+
+            bpy.ops.object.modifier_apply(modifier='bool3')
+
+            # Export the united mesh as an STL file
+            output_file = 'parts/part_' + str(k) + '.stl' 
+            bpy.ops.export_mesh.stl(filepath=output_file, use_selection=True)
+
+            print('part_' + str(k) + '.stl' + ' created')
+
+
 
 
